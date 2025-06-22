e77197ee6de1714e9bf9b70d633ab1e85784bcfc
Abdul Rehman <137405437+AbdulRehman938@users.noreply.github.com>
2025-06-22 22:52:09 +0500

    solve10

--- src/constants/Login/socialButtons.js
+++ src/constants/Login/socialButtons.js
@@ -1,13 +1,11 @@
-import gologin from "../../assets/images/Googlelogin.svg"
-import applelogin from "../../assets/images/Applelogin.svg"
-import facelogin from "../../assets/images/facebooklogin.svg"
-
-
+import gologin from "../../assets/images/Googlelogin.svg";
+import applelogin from "../../assets/images/Applelogin.svg";
+import facelogin from "../../assets/images/facebooklogin.svg";
 
 const socialButtons = [
-  { img: {gologin}, text: 'Continue with Google' },
-  { img: {applelogin}, text: 'Continue with Apple' },
-  { img: {facelogin}, text: 'Continue with Facebook' }
+  { img: gologin, text: 'Continue with Google' },
+  { img: applelogin, text: 'Continue with Apple' },
+  { img: facelogin, text: 'Continue with Facebook' }
 ];
 
 export default socialButtons;
